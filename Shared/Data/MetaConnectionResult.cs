e4c37f916a493f843f91d2fc7989cb2dbae39178
mefodei <mefodei.link@gmail.com>
2024-06-25 02:57:56 +0700

    - update meta back

--- Shared/Data/MetaConnectionResult.cs
+++ Shared/Data/MetaConnectionResult.cs
@@ -8,5 +8,10 @@
         public bool Success;
         public string Error;
         public ConnectionState State;
+
+        public override string ToString()
+        {
+            return $"Success: {Success}, Error: {Error}, State: {State}";
+        }
     }
 }
