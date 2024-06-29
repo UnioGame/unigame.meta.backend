75a6f4256a89099996932ece4bc0b0e278b62d52
mefodei <mefodei.link@gmail.com>
2024-06-29 21:39:27 +0700

    - update remote constants

--- ModelMapping/RemoteMetaConstants.cs
+++ ModelMapping/RemoteMetaConstants.cs
@@ -2,6 +2,7 @@
 {
     public class RemoteMetaConstants
     {
+        public const string ContractRemove = "Contract";
         public const string RemoteCommandTemplate = "Command";
         public const string DefaultMethodTemplate = "Get{0}";
         public const string DefaultMethod = "InvokeRemote";
--- ModelMapping/RemoteMetaDataConfig.cs
+++ ModelMapping/RemoteMetaDataConfig.cs
@@ -4,35 +4,24 @@
     using MetaService.Shared;
     using Sirenix.OdinInspector;
     using UnityEngine;
-    using UnityEngine.Serialization;
 
     [Serializable]
     public class RemoteMetaDataConfig : IRemoteMetaDataConfiguration
     {
         [SerializeReference]
         public IRemoteDataConverter defaultConverter = new JsonRemoteDataConverter();
-        
-        [FormerlySerializedAs("remoteGetMethodTemplate")]
-        public string getMethodTemplate = "Get{0}";
-        [FormerlySerializedAs("remotePostMethodTemplate")]
-        public string postMethodTemplate = "Post{0}";
-        
+
         [Searchable(FilterOptions = SearchFilterOptions.ISearchFilterableInterface)]
         [ListDrawerSettings(ListElementLabelName = "name")]
         public RemoteMetaCallData[] remoteMetaData = Array.Empty<RemoteMetaCallData>();
 
         public IRemoteDataConverter Converter => defaultConverter;
-        public string GetMethodTemplate => getMethodTemplate;
-        public string CommandMethodTemplate => postMethodTemplate;
         public RemoteMetaCallData[] RemoteMetaData => remoteMetaData;
         
         
         public string GetContractName(IRemoteCallContract contract)
         {
-            var inputType = contract.InputType;
-            var outputType = contract.OutputType;
-            var contractName = $"out: {outputType.Name} arg: {inputType.Name}";
-            return contractName;
+            return GetRemoteMethodName(contract);
         }
         
         public string GetRemoteMethodName(IRemoteCallContract contract)
@@ -40,41 +29,10 @@
             if (string.IsNullOrEmpty(contract.MethodName) == false)
                 return contract.MethodName;
             
-            var outputType = contract.OutputType;
-            var inputType = contract.InputType;
-            var targetType = outputType;
-            
-            var isPostCommand = false;
-            
-            if (IsEmptyType(outputType) == false)
-            {
-                targetType = outputType;
-                isPostCommand = false;
-            }
-            else if (IsEmptyType(inputType) == false)
-            {
-                targetType = inputType;
-                isPostCommand = true;
-            }
-            else
-            {
-                targetType =  typeof(VoidRemoteData);
-            }
-            
-            if(IsEmptyType(targetType))
-                return RemoteMetaConstants.DefaultMethod;
-            
-            var typeName = targetType.Name;
-            
-            var methodTemplate = isPostCommand
-                ? postMethodTemplate
-                : getMethodTemplate;
-                
-            methodTemplate = string.IsNullOrEmpty(methodTemplate)
-                ? RemoteMetaConstants.DefaultMethodTemplate
-                : methodTemplate;
-            
-            return string.Format(methodTemplate, typeName);
+            var contractType = contract.GetType();
+            var contractName = contractType.Name;
+            contractName = contractName.Replace(RemoteMetaConstants.ContractRemove, string.Empty);
+            return contractName;
         }
 
         public bool IsEmptyType(Type type)
