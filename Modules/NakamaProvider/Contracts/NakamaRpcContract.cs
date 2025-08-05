2af186008f392560fc9aa4123ae108e922db164f
Mefodei <mefodei.link@gmail.com>
2025-08-05 13:03:42 +0700

    - update rpc methods

---
+++ Modules/NakamaProvider/Contracts/NakamaRpcContract.cs
@@ -0,0 +1,16 @@
+﻿namespace UniGame.MetaBackend.Runtime.Contracts
+{
+    using System;
+    using Shared;
+
+    [Serializable]
+    public class NakamaRpcContract : RemoteMetaContract<string, string> ,INakamaContract
+    {
+        public string rpcName = string.Empty;
+        public string payload = string.Empty;
+        
+        public override string Path => rpcName;
+        
+        public override object Payload => payload;
+    }
+}
