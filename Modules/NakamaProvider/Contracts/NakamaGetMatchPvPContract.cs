fd3621590431a02a2f04cec289e0ba1a00069dff
OLN <oleghack@gmail.com>
2026-02-06 13:54:28 +0700

    - add match pvp contract

---
+++ Modules/NakamaProvider/Contracts/NakamaGetMatchPvPContract.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Newtonsoft.Json;
+using UniGame.MetaBackend.Runtime;
+
+[Serializable]
+public class NakamaGetMatchPvPContract : NakamaContract<NakamaGetMatchPvPContract,NakamaGetMatchPvPResult>
+{    
+  [JsonIgnore]
+  public override string Path => nameof(NakamaGetMatchPvPContract);
+}
+
+public class NakamaGetMatchPvPResult
+{
+  public bool success;
+}
